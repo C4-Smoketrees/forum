bdebbd90e47bd67596e715ff90ff922bcb60993f
SilverSpear28 <abhijaymessi@gmail.com>
2020-04-25 15:29:31 +0530

    Changes made

--- src/components/entrypoints/create-blog.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-import { hydrate } from 'react-dom'
-
-import CreateBlog from '../pages/create-blog/create-blog.jsx'
-
-hydrate(<CreateBlog />, document.getElementById('root'))

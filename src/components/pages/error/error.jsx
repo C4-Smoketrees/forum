e16311986d0fb4bc89c2b532f670f95f994e5637
SilverSpear28 <abhijaymessi@gmail.com>
2020-04-28 19:02:03 +0530

    add Navbar

--- src/components/pages/error/error.jsx
+++ src/components/pages/error/error.jsx
@@ -1,13 +1,62 @@
 import React from 'react'
+import Navbar from '../../navbar/navbar'
+import SideDrawer from '../../side-drawer/side-drawer'
+import Backdrop from '../../backdrop/backdrop'
 
 class Error extends React.Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      navbarOpen: true,
+      loading: true,
+      sideDrawerOpen: false,
+      isLoggedIn: false
+    }
+
+    this.user = {
+      name: 'Shizuka',
+      descriptionUser: 'FullStackDev',
+      Hash: {
+        number: 1,
+        arrayHash: ['#Dev']
+      },
+      likes: 420,
+      issues: 69,
+      issuesSolved: 0,
+      photo: 'https://i0.wp.com/www.universodegatos.com/wp-content/uploads/2017/04/fivfelv7.jpg',
+      emailId: 'example@gmail.com',
+      time: '4 hrs ago'
+    }
+
+    this.handleToggleClick = () => {
+      this.setState((pevState) => {
+        return { sideDrawerOpen: !pevState.sideDrawerOpen } // passing reference
+      })
+    }
+
+    this.onBackdropClick = () => {
+      this.setState({ sideDrawerOpen: false })
+    }
+  }
+
   render () {
+    let backdrop
+
+    if (this.state.sideDrawerOpen) {
+      backdrop = <Backdrop handleOnClick={this.onBackdropClick} />
+    }
     return (
-      <div className='error'>
-        <img src='http://localhost:3000/svg/error.svg' />
-        <h1>Oops!</h1>
-        <div className='error-description'>
+      <div className='center' style={{ height: '100%', paddingBottom: '20px' }}>
+        <Navbar onToggleClick={this.handleToggleClick} user={this.user} />
+        <SideDrawer show={this.state.sideDrawerOpen} user={this.user} />
+        {backdrop}
+        <div className='error'>
+          <img src='http://localhost:3000/svg/error.svg' />
+          <h1>Oops!</h1>
+          <div className='error-description'>
           An error occured! Thats embarassing!
+          </div>
         </div>
       </div>
     )
